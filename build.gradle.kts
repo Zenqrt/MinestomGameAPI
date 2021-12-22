3b40428e07b4dd93b21e77b75be1cf14c813a993
Zenqrt <craftybuilder311@gmail.com>
2021-12-22 17:57:50 -0500

    Update Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.0"
     java
 }
